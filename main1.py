b6cae9ad95348c591fdf5834ab4e2a1bf93e6cfb
vicysmalls <120691212+vicysmalls@users.noreply.github.com>
2024-04-16 13:48:25 -0400

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,32 @@
+from Simulation import*
+
+def init_surface(size, caption):
+    pygame.init()
+    pygame.display.set_caption(caption)
+    surface = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    return surface, clock
+
+def run():
+    width, height = 600, 600
+    fps = 60
+
+    surface, clock = init_surface((width, height), 'Simple Pendulum')
+    simulation = Simulation(width, height)
+    stop = False
+
+    while not stop:
+        clock.tick(fps)
+        surface.fill(black)
+
+        for event in pygame.event.get():
+            stop = event.type == pygame.QUIT
+
+        simulation.moving_logic(surface)
+        simulation.collision_logic()
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+run()
